898937d249dc5583f5b86f1da67a244da83e6530
Alexander <Alexander.GoldenSoft@gmail.com>
2010-07-15 08:47:24 +0000

    Базовый интерфейс фискального регистратора

---
+++ Base_FiscalRegister_unit.pas
@@ -0,0 +1,58 @@
+unit Base_FiscalRegister_unit;
+
+{
+  базовый интерфейс, который реальзует ф-ционал абстрактного
+  фискального регистратора
+}
+
+interface
+
+uses
+  Classes, Windows, kbmMemTable, DB, Front_DataBase_Unit;
+
+type
+  IBaseFiscalRegister = interface
+  ['{308C2D25-B5F7-4801-B8E4-F09E92B7AFBA}']
+    // Проверка устройства
+    function CheckDeviceInfo: Boolean;
+    // Инициализация
+    function Init: Boolean;
+    // Печать чека
+    function PrintCheck(const Doc, DocLine, PayLine: TkbmMemTable): Boolean;
+
+    // Установка рабочей базы
+    procedure SetFrontBase(const Value: TFrontBase);
+    function GetFrontBase: TFrontBase;
+    property FrontBase: TFrontBase read GetFrontBase write SetFrontBase;
+
+    //отчет Z1 с гашением
+    function PrintZ1ReportWithCleaning: Boolean;
+    //отчет Z2 с гашением
+    function PrintZ2ReportWithCleaning: Boolean;
+    //отчет X1 без гашения
+    function PrintX1ReportWithOutCleaning: Boolean;
+    //отчет X2 без гашения
+    function PrintReportWithOutCleaning2: Boolean;
+    procedure OpenDrawer;
+    procedure EndSession;
+    //деньги в кассу
+    procedure MoneyIn;
+    //деньги из кассы
+    procedure MoneyOut;
+    function GetDocumentNumber: Integer;
+    //возврат ошибки
+    procedure ErrMessage(Err: Integer);
+  end;
+
+var
+  BaseFiscalRegister: IBaseFiscalRegister;
+
+implementation
+
+
+initialization
+  BaseFiscalRegister := nil;
+
+finalization
+
+end.
